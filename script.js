669ef501ecce254caf27f116f7422428d33fc074
Ananththandavarayamurthi <110927979+Ananththandavarayamurthi@users.noreply.github.com>
2022-11-12 22:31:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -40,7 +40,7 @@ document.body.append(section)
                 id.innerHTML =`<h4>loading weather....</h4>`
                 try{
                   const res =await fetch(
-                    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lan}&appid=367261ea67167733e6e17d88b7110e22`
+                    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lan}&appid=367261ea67167733e6e17d88b7110e22&units=metric`
                   );
                  
                   const wdata= await res.json();
